e481c7f4619fbd029c6e29de0950bbfc79260d46
on9au <151502370+on9au@users.noreply.github.com>
2026-09-15 13:29:50 +1000

    feat: add graph alias command to git

--- modules/programs/tools/git/home.nix
+++ modules/programs/tools/git/home.nix
@@ -1,6 +1,8 @@
 {...}: {
   programs.git.enable = true;
 
+  programs.git.settings.alias.graph = "log --all --decorate --oneline --graph";
+
   programs.gh = {
     enable = true;
     gitCredentialHelper.enable = true;
